0413f530e014f05eeca54a4c35055e1afff43618
Keegan Liu <k342liu@uwaterloo.ca>
2022-11-30 01:19:30 -0500

    Now produces final lofi-beat

--- dl_five_top_tracks.py
+++ dl_five_top_tracks.py
@@ -20,7 +20,12 @@ import os
 import unicodedata
 import re
 
-# For downloading mp3s
+# BPM Calculations
+from aubio import source, tempo
+from numpy import median, diff
+
+# Vocal manipulations
+from pydub import AudioSegment
 
 
 class MyLogger(object):
@@ -44,7 +49,7 @@ ydl_opts = {
     'format': 'bestaudio/best',
     'postprocessors': [{
         'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
+        'preferredcodec': 'wav',
         'preferredquality': '192',
     }],
     'logger': MyLogger(),
@@ -57,19 +62,19 @@ ydl_opts = {
 def download_audio(yt_url):
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         video_info = ydl.extract_info(url=yt_url, download=False)
-        filename = f"{video_info['id']}.mp3"
-        print(f"DOWNLOADING... Current file (yt_id.mp3): {filename}")
+        filename = f"{video_info['id']}.wav"
+        print(f"DOWNLOADING {filename}...")
         # print(f"DURATION: {video_info['duration']} seconds")
-        print(list(video_info))
+        # print(list(video_info)) # TO SEE FIELDS WE CAN READ...
         ydl.download([yt_url])
         return video_info['id']
 
 
-def removeBackgroundFromMp3File(file_name):
-    file_path = f"./audio_files/{file_name}.mp3"
+def removeBackgroundFromAudioFile(file_name):
+    file_path = f"./audio_files/{file_name}.wav"
     audio = AudioFileClip(file_path)
-    audio.write_audiofile(f'./output/{file_name}.mp3')
-    os.system(f'spleeter separate ./output/{file_name}.mp3 -o output')
+    audio.write_audiofile(f'./output/{file_name}.wav')
+    os.system(f'spleeter separate ./output/{file_name}.wav -o output')
 
 
 def replaceInvalidChars(file_name):
@@ -77,6 +82,81 @@ def replaceInvalidChars(file_name):
         'ascii', 'ignore').decode('ascii')
     file_name = re.sub(r'[^\w\s-]', '', file_name.lower())
     return re.sub(r'[-\s]+', '-', file_name).strip('-_')
+
+
+def get_file_bpm(path, params=None):
+    """ Calculate the beats per minute (bpm) of a given file.
+        path: path to the file
+        param: dictionary of parameters
+    """
+    if params is None:
+        params = {}
+    # default:
+    samplerate, win_s, hop_s = 44100, 1024, 512
+    if 'mode' in params:
+        if params.mode in ['super-fast']:
+            # super fast
+            samplerate, win_s, hop_s = 4000, 128, 64
+        elif params.mode in ['fast']:
+            # fast
+            samplerate, win_s, hop_s = 8000, 512, 128
+        elif params.mode in ['default']:
+            pass
+        else:
+            raise ValueError("unknown mode {:s}".format(params.mode))
+    # manual settings
+    if 'samplerate' in params:
+        samplerate = params.samplerate
+    if 'win_s' in params:
+        win_s = params.win_s
+    if 'hop_s' in params:
+        hop_s = params.hop_s
+
+    s = source(path, samplerate, hop_s)
+    samplerate = s.samplerate
+    o = tempo("specdiff", win_s, hop_s, samplerate)
+    # List of beats, in samples
+    beats = []
+    # Total number of frames read
+    total_frames = 0
+
+    while True:
+        samples, read = s()
+        is_beat = o(samples)
+        if is_beat:
+            this_beat = o.get_last_s()
+            beats.append(this_beat)
+            # if o.get_confidence() > .2 and len(beats) > 2.:
+            #    break
+        total_frames += read
+        if read < hop_s:
+            break
+
+    def beats_to_bpm(beats, path):
+        # if enough beats are found, convert to periods then to bpm
+        if len(beats) > 1:
+            if len(beats) < 4:
+                print("few beats found in {:s}".format(path))
+            bpms = 60./diff(beats)
+            return median(bpms)
+        else:
+            print("not enough beats found in {:s}".format(path))
+            return 0
+
+    return beats_to_bpm(beats, path)
+
+
+def speed_change(sound, speed=1.0):
+    # Manually override the frame_rate. This tells the computer how many
+    # samples to play per second
+    sound_with_altered_frame_rate = sound._spawn(sound.raw_data, overrides={
+        "frame_rate": int(sound.frame_rate * speed)
+    })
+
+    # convert the sound with altered frame rate to a standard frame rate
+    # so that regular playback programs will work right. They often only
+    # know how to play audio at standard frame rate (like 44.1k)
+    return sound_with_altered_frame_rate.set_frame_rate(sound.frame_rate)
 
 
 # Spotify Keys
@@ -89,35 +169,37 @@ client_credentials_manager = SpotifyClientCredentials(
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 # Global Top Songs URI
-playlist_URI = "37i9dQZEVXbNG2KDcFcKOF"
+# PLAYLIST_URI = "37i9dQZEVXbNG2KDcFcKOF"
+PLAYLIST_URI = "56j8LKCvUH4kyEHHQW5Dge"
 track_uris = [x["track"]["uri"]
-              for x in sp.playlist_tracks(playlist_URI)["items"]]  # List of track URIs
+              for x in sp.playlist_tracks(PLAYLIST_URI)["items"]]  # List of track URIs
 # print(track_uris)
 # print("\n\n")
 
 top_fifty_tracks = []
 five_rand_tracks = []
-tracks_and_ids = {}
+tracks_data_struct = {}
 
-for track in sp.playlist_tracks(playlist_URI)["items"]:
+for track in sp.playlist_tracks(PLAYLIST_URI)["items"]:
 
     # Track name
+    # print(track["track"])
     track_name = track["track"]["name"]
     # print(track_name)
 
     # Main Artist
-    artist_uri = track["track"]["artists"][0]["uri"]
-    artist_info = sp.artist(artist_uri)
+    #  artist_uri = track["track"]["artists"][0]["uri"]
+    #  artist_info = sp.artist(artist_uri)
     # print(artist_info)
 
     # Name, popularity, genre
     artist_name = track["track"]["artists"][0]["name"]
-    artist_genres = artist_info["genres"]
+    # artist_genres = artist_info["genres"]
     # print(artist_name)
     # print(artist_genres)
 
     # Popularity of the track
-    track_pop = track["track"]["popularity"]
+    # track_pop = track["track"]["popularity"]
     # print(track_pop)
 
     top_fifty_tracks.append(replaceInvalidChars(
@@ -128,26 +210,24 @@ for i, track in enumerate(top_fifty_tracks, start=1):
     print(f"Track {i}: {track}")
 print("\n")
 
-# for i in range(1, 6):
-#     rand_track = top_fifty_tracks.pop(randrange(len(top_fifty_tracks)))
-#     five_rand_tracks.append(rand_track)
-#     tracks_and_ids[rand_track] = ""
-# print("\n")
+for i in range(1, 6):
+    rand_track = top_fifty_tracks.pop(randrange(len(top_fifty_tracks)))
+    five_rand_tracks.append(rand_track)
+print("\n")
 
 # FOR TESTING PURPOSES:
-five_rand_tracks.append("anti-hero-by-taylor-swift")
-five_rand_tracks.append("rich-flex-by-drake")
-five_rand_tracks.append("unholy-feat-kim-petras-by-sam-smith")
-five_rand_tracks.append("major-distribution-by-drake")
-five_rand_tracks.append("pussy-millions-feat-travis-scott-by-drake")
+# five_rand_tracks.append("anti-hero-by-taylor-swift")
+# five_rand_tracks.append("rich-flex-by-drake")
+# five_rand_tracks.append("unholy-feat-kim-petras-by-sam-smith")
+# five_rand_tracks.append("major-distribution-by-drake")
+# five_rand_tracks.append("pussy-millions-feat-travis-scott-by-drake")
+# five_rand_tracks.append("not-the-only-one-by-sam-smith")
+# five_rand_tracks.append("too-good-at-goodbyes-by-sam-smith")
 
 
 print("CHOSEN 5 TRACKS: ")
 for i, track in enumerate(five_rand_tracks, start=1):
     print(f"Track {i}: {track}")
-
-print("NEW OBJECT: ")
-print(tracks_and_ids)
 print("\n")
 
 for i, track in enumerate(five_rand_tracks, start=1):
@@ -162,25 +242,60 @@ for i, track in enumerate(five_rand_tracks, start=1):
 
     # Download each track
     vid_id = download_audio(yt_link)
-    tracks_and_ids[track] = vid_id
-    print(tracks_and_ids)
+    tracks_data_struct[track] = {'id': vid_id}
+    print(tracks_data_struct)
 
     # Separate vocals from instrumental
-    removeBackgroundFromMp3File(vid_id)
+    removeBackgroundFromAudioFile(vid_id)
 
     print("\n")
 
 # Navigate to the output directory
 os.chdir("output")
-for track in tracks_and_ids:
-    yt_id = tracks_and_ids[track]
+
+for track in tracks_data_struct:
+    yt_id = tracks_data_struct[track]['id']
     for filename in os.listdir():
-        print(f"Current ID: {yt_id}")
-        print(f"Current file: {filename}")
-        if yt_id in filename and filename.endswith(".mp3"):
-            print(f"Renaming... {filename} -> {track}.mp3")
-            os.rename(filename, f"{track}.mp3")
+        # Rename audio files
+        if yt_id in filename and filename.endswith(".wav"):
+            print(f"Renaming... {filename} -> {track}.wav")
+            os.rename(filename, f"{track}.wav")
+        # Rename folders
         elif yt_id in filename:
             print(f"Renaming... {filename} -> {track}")
             os.rename(filename, track)
-        print("\n")
+print("\n")
+
+# Calculate the BPM for each track and add to data struct
+print("Calculating BPM...")
+for track in tracks_data_struct:
+    current_file = f"{track}.wav"
+    bpm = get_file_bpm(f"./{current_file}")
+    print(f"Current file: {current_file}  BPM: {bpm}")
+
+    tracks_data_struct[track]['bpm'] = bpm
+print(tracks_data_struct)
+print("\n")
+
+# Slow vocals accordingly
+print("Slowing vocals...")
+lofi_beat = AudioSegment.from_file("../beats_library/85BPM.mp3") - 8
+beat_bpm = 85
+for track in tracks_data_struct:
+    vocals = AudioSegment.from_file(f"./{track}/vocals.wav")
+    song_bpm = tracks_data_struct[track]["bpm"]
+
+    # Slow the vocals to achieve the same bpm as the beat
+    slow_vocals = speed_change(vocals, beat_bpm / song_bpm)
+    slow_vocals.export(f"./{track}/slow_vocals.mp3", format="mp3")
+
+    # Pitch down slow_vocals.mp3 to create slow_and_low_vocals.mp3 using command line
+    os.system(
+        f"ffmpeg -i ./{track}/slow_vocals.mp3 -af asetrate=44100*0.7,aresample=44100,atempo=1/0.7 ./{track}/slow_and_low_vocals.mp3")
+    slow_and_low_vocals = AudioSegment.from_file(
+        f"./{track}/slow_and_low_vocals.mp3")
+
+    # Create the track by overlaying the beat over the slow vocals
+    #### EDITED HERE ####
+    lofi_track = slow_vocals.overlay(lofi_beat, position=0)
+    lofi_track.export(f"./{track}/final_lofi.mp3", format="mp3")
