e1f63fb71997ba65e3bb98112714bd3c040f6ccb
Cindy Peng <cindypeng80@gmail.com>
2022-12-02 16:07:44 -0500

    video and audio merger defined

--- backgroundvisuals.py
+++ backgroundvisuals.py
@@ -8,20 +8,19 @@ import time
 import os.path 
 import glob
 import shutil
+import moviepy.editor as mpe
 
 #Downloads livewallpaper
 options = Options()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
-
 driver = webdriver.Chrome(executable_path=r"D:\SW\chromedriver90\chromedriver.exe",chrome_options=options)
 
 url= "https://mylivewallpapers.com/nature/evening-waterfall-live-wallpaper/"
-
 driver.get(url)
-
 download_button = driver.find_element(By.XPATH,'/html/body/main/div[3]/div[2]/div/div[3]/div[5]/div[1]/ul/li[1]/div[2]/div/div/div/div/div[3]/a ')
 download_button.click()
 
+time.sleep(8)
 driver.quit()
 
 
@@ -30,12 +29,21 @@ path = (r"C:\Users\Cindy Peng\Downloads")
 new_path = (r"C:\Users\Cindy Peng\Documents\Code\mewzaki\background")
 
 folder_path = (r"C:\Users\Cindy Peng\Downloads")
-file_type = (r"\*crdownload")
+file_type = (r"\*mp4")
 files = glob.glob(folder_path + file_type)
 max_file = max(files, key=os.path.getctime)
 
 shutil.move(max_file,new_path)
 
+
+def combine(vidname, audname, outname, fps=60):
+    my_clip = mpe.VideoFileClip(vidname)
+    audio_background = mpe.AudioFileClip(audname)
+    final_clip = my_clip.set_audio(audio_background)
+    final_clip.write_videofile(outname, fps=fps)
+
+
+combine("mylivewallpapers-com-Evening-Waterfall-4K (1).mp4","kendrick.mp3", "test.mp4")    
 
 
 #loop the mp4 to meet song length
